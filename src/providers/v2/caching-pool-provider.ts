20b808d2ae6836242b47de1d852b9ea4020196e0
sotatek <sotatek@sotatek-hieu.tran2>
2023-04-12 11:21:48 +0700

    Update package v2 sdk

--- src/providers/v2/caching-pool-provider.ts
+++ src/providers/v2/caching-pool-provider.ts
@@ -1,5 +1,5 @@
 import { Token } from '@uniswap/sdk-core';
-import { Pair } from '@xswap-v3/v2-sdk';
+import { Pair } from '@uniswap/v2-sdk';
 import _ from 'lodash';
 
 import { ChainId } from '../../util/chains';
--- src/routers/router.ts
+++ src/routers/router.ts
@@ -13,7 +13,7 @@ import {
   TradeType,
 } from '@uniswap/sdk-core';
 import { SwapOptions as UniversalRouterSwapOptions } from '@uniswap/universal-router-sdk';
-import { Route as V2RouteRaw } from '@xswap-v3/v2-sdk';
+import { Route as V2RouteRaw } from '@uniswap/v2-sdk';
 import {
   Pool,
   Position,
